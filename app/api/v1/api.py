e27394de5ae17bf843f389034d304a10465716a9
jesna <j.jose@nextmovebs.com>
2026-09-13 12:03:09 +0300

    Fix stylist endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -5,5 +5,6 @@ api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(wardrobe.router, prefix="/wardrobe", tags=["Wardrobe"])
+api_router.include_router(outfits.router, prefix="/stylist", tags=["Stylist"])
 api_router.include_router(outfits.router, prefix="/outfits", tags=["Outfits"])
 api_router.include_router(seed.router, prefix="/dev", tags=["Developer Services"])
