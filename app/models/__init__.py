44cacbe5a071d3a9a12ffe74a7039512aa594415
jesna <j.jose@nextmovebs.com>
2026-09-14 15:24:50 +0300

    Fix user data

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
-from app.models.wardrobe import WardrobeItem, Category
 from app.models.user import User
+from app.models.user_profile import UserProfile
+from app.models.wardrobe import Category, WardrobeItem
 
-__all__ = ["WardrobeItem", "Category", "User"]
+__all__ = ["User", "UserProfile", "WardrobeItem", "Category"]
